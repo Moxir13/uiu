4357145f1d4e30dd5498420524f3dab624247255
Moxir13 <r9294982@gmail.com>
2025-08-13 09:12:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const odam ={
+    name: null,
+    id: null,
+    password: null,
+};
+odam.name = prompt("ism kiriting");
+odam.id = prompt("space ID kriting");
+odam.password = prompt("Parol kriting");
+console.log(odam);
